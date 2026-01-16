bf3fd805f227fcb4c8cc54ec2a7c86911492598c
Raad05 <yamin.raad6109@gmail.com>
2026-01-16 18:22:53 +0600

    mutable, immutable and constant variables

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
-    println!("Hello, world!");
+    // immutable variable
+    let x = 10;
+    println!("Value of x: {}", x);
+
+    // mutable variable
+    let mut y: i32 = 10;
+    println!("Before change: {}", y);
+
+    y = 5;
+    println!("After change: {}", y);
+
+    // constant variable
+    const HOUR_IN_SECONDS: u32 = 1 * 60 * 60;
+    println!("Number of seconds in an hour: {}", HOUR_IN_SECONDS);
 }
